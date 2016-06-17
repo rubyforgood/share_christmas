3df9d026f998ccd2889094dc67ad764a0c95bf7a
Alex Egan <alexander.egan@customink.com>
2016-06-17 13:09:12 -0400

    Add RSpec
    Moves to RSpec for the test suite. This installs rspec rails using the
    `rails generate rspec:install` command. I also added configuration for
    rspec to filter for the `focus` option if found. The default rake task
    is now to run rspec, but you can run the specs with `rspec` as well.

---
+++ spec/models/organization_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Organization do
+  # it 'tests the truth' do
+  #   expect(true).to be true
+  # end
+end
---
+++ spec/models/volunteer_center_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe VolunteerCenter do
+  # it 'tests the truth' do
+  #   expect(true).to be true
+  # end
+end
