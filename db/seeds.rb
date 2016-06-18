35abf517101b77ca2cac7cba32de47f4188ab41f
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-06-18 14:07:39 -0400

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,14 @@ u.add_role('volunteer_center_admin')
 
 User.create!(email: ENV['USER_EMAIL'], password: ENV['USER_PASSWORD'], password_confirmation: ENV['USER_PASSWORD'])
 
-VolunteerCenter.create(name: 'Durham, NC Volunteer Center')
+vc = VolunteerCenter.create(name: 'Durham, NC Volunteer Center')
+
+oo = vc.organizations.create!(name: "Aldersgate United Methodist Church", description: "Church", url: "https://shareyourchristmas.net/partner/aldersgate/4")
+
+cp = vc.campaigns.create!(name: "Share Christmas 2016", donation_deadline: Date.today + 5.days, reminder_date: Date.today + 2.days, description: "Christmas campaign")
+
+oc = OrganizationCampaign.create!(organization: oo, campaign: cp)
+
+10.times do |i|
+  Recipient.create!(first_name: "John", last_name: "Doe", email: "JohnDoe#{i}@gmail.com", street: "Main Ave", city: "Springfield", state: "VA", zip_code: "22012", age: 25, gender: "male", race: "Hispanic", size: "M", wish_list: "Generic")
+end
