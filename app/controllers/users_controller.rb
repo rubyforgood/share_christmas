6671acf22ebb562888c718a8ab90fe790a87e90d
Nick Olds <nolds9@yahoo.com>
2016-06-18 16:39:55 -0400

    adds user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def styleguide
     @user = User.new
+  end
+
+  def show
+    @memberships = current_user.memberships
   end
 end
