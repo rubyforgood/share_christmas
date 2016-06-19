4fff16cf4613f7b1d37fb5579fa08e5b7d9c65eb
Nick Olds <nolds9@yahoo.com>
2016-06-18 21:21:19 -0400

    hotfix: fixes bug with admin views

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -119,7 +119,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+   config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -129,7 +129,7 @@ ActiveAdmin.setup do |config|
   # config.comments_order = 'created_at ASC'
   #
   # You can disable the menu item for the comments index page:
-  # config.comments_menu = false
+   config.comments_menu = false
   #
   # You can customize the comment menu:
   # config.comments_menu = { parent: 'Admin', priority: 1 }
