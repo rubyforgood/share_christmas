2816a8d503ef1a6ccd44a9eefb7e9b5029a3d3d1
Alex Egan <alexander.egan@customink.com>
2016-06-19 10:22:10 -0400

    create a match in the matches controller
    authorizes the user by finding the membership, creates the match by the
    membership id and the recipient id.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,19 @@
 class MatchesController < ApplicationController
-  def create
+  before_action :authorize_by_membership, only: :create
 
+  def create
+    match = Match.create(recipient_id: @recipient.id, membership_id: @membership.id)
     redirect_to user_show_path
+  end
+
+  private
+  def authorize_by_membership
+    @recipient = Recipient.find(params[:match][:recipient_id])
+    organization_id = @recipient.organization_campaign.organization_id
+    @membership = Membership.where(user_id: current_user.id, organization_id: organization_id).first
+    if @membership.nil?
+      flash[:warning] = 'You must be a member of the organization to donate.'
+      redirect_to :root
+    end
   end
 end
