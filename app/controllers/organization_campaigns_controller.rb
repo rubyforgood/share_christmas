5eeeb96d6cbc3af16566cfa047dafe148e9a5771
Alexis Johnson <alexis.johnson052@gmail.com>
2016-06-19 11:04:59 -0400

    store campaign info in session (#77)

--- app/controllers/organization_campaigns_controller.rb
+++ app/controllers/organization_campaigns_controller.rb
@@ -1,9 +1,19 @@
 class OrganizationCampaignsController < ApplicationController
+  before_action :store_campaign
   before_action :authenticate_user!
+
   def show
-    @organization_campaign = OrganizationCampaign.find(params[:id])
     @recipients = @organization_campaign.recipients
     @donors = @organization_campaign.organization.memberships
+  end
+
+  private
+
+  def store_campaign
+    @organization_campaign = OrganizationCampaign.find(params[:id])
+    session[:organization_name] = @organization_campaign.organization_name
+    session[:campaign_name] = @organization_campaign.campaign_name
+    session[:campaign_logo_url] = @organization_campaign.campaign_logo.url
   end
 
 end
