67ae266a18b2eb6d0e87df2e9560ab964f7b2375
Craig Riecke <cr396@cornell.edu>
2016-06-18 21:08:55 -0400

    Fix migration

--- db/migrate/20160619005425_drop_active_admin_comments.rb
+++ db/migrate/20160619005425_drop_active_admin_comments.rb
@@ -1,11 +1,9 @@
 class DropActiveAdminComments < ActiveRecord::Migration
-  def change
-    def up
-      drop_table :active_admin_comments
-    end
+  def up
+    drop_table :active_admin_comments
+  end
 
-    def down
-      raise ActiveRecord::IrreversibleMigration
-    end
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
