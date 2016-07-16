7f2399a223c4af0e7e97cedc7119660a9a1cb1fc
Craig Riecke <cr396@cornell.edu>
2016-07-16 14:36:17 -0400

    Add member functionality

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,10 +7,47 @@ class MembershipsController < ApplicationController
     @memberships = @org.memberships.includes(:user).order("users.last_name, users.first_name")
   end    
 
+  def new
+    @user = User.new
+    @membership = Membership.new
+  end
+
+  def create
+    # Create the user first, but don't duplicate!  Generate a phony password - this will 
+    # be overwritten on the user's first login
+    nup = new_user_params
+    user = User.find_by(email: nup[:email])
+    unless user 
+      # Create user.
+      nup[:password] = nup[:password_confirmation] = ENV['USER_PASSWORD'] 
+      user = User.create!(nup.except(:membership))
+
+      # TODO: If send_email is checked, send a reset password email 
+    end
+
+    # if user is already a member, don't create another record
+    m = Membership.find_by(organization: @org, user: user)
+    unless m
+      Membership.create!(
+        organization: @org, 
+        user: user, 
+        send_email: nup[:membership][:send_email]
+      )
+    end
+    redirect_to organization_memberships_path(@org.id)
+  end
+
 private
   def load_org_and_authorize 
     @org = Organization.friendly.find(params[:organization_id])
     authorize! :admin, @org
   end 
+
+  def new_user_params
+    params.require(:user).permit(
+      :first_name, :last_name,:email,
+      membership: [:send_email]
+    )
+  end
 
 end
--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -28,4 +28,61 @@ RSpec.describe MembershipsController, type: :controller do
       end
     end
   end
+
+  describe "new >" do
+    it "will get a fresh user and membership record" do
+      subject.current_user.add_role(:admin, org) 
+
+      get :new, organization_id: org.id
+      expect(assigns(:user)).to be_a_new(User)
+      expect(assigns(:membership)).to be_a_new(Membership)
+    end
+  end
+
+  describe "create >" do
+    let(:willy_smith_attr) { 
+      {first_name:"Willy", last_name:"Smith", email:"wsmith@gmail.com",
+        membership: {send_email: true}
+      } 
+    }
+    let(:willy_smith_user_attr) {willy_smith_attr.except(:membership)}
+    before(:each) { subject.current_user.add_role(:admin, org) }
+
+    it "creates a new user, if a user with that email doesn't exist" do
+      expect {
+        post :create, organization_id: org.id, user: willy_smith_attr
+      }.to change {User.count}.by 1
+    end
+
+    it "uses existing user, if it does exist" do
+      # Need password to create a new user from scratch
+      willy_smith_attr[:password] = willy_smith_attr[:password_confirmation] = "BLUGGGGH"
+      ws = User.create!(willy_smith_user_attr)
+      expect {
+        post :create, organization_id: org.id, user: willy_smith_attr
+      }.to_not change {User.count}
+    end
+
+    it "creates a membership, if it doesn't exist" do
+      expect {
+        post :create, organization_id: org.id, user: willy_smith_attr
+      }.to change {Membership.count}.by 1
+      expect(response).to redirect_to organization_memberships_path(org.id)
+    end
+
+    it "doesn't create a membership, if it already exists" do
+      willy_smith_attr[:password] = willy_smith_attr[:password_confirmation] = "BLUGGGGH"
+      ws = User.create!(willy_smith_user_attr)
+      Membership.create!(organization: org, user: ws)
+
+      expect {
+        post :create, organization_id: org.id, user: willy_smith_attr
+      }.to_not change {Membership.count}
+      expect(response).to redirect_to organization_memberships_path(org.id)
+    end
+
+
+  end
+
+
 end
