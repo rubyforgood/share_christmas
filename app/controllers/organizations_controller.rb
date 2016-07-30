34cd9f87c1c2931ada597857b31dae71a82dbd29
Alex Egan <alexander.egan@customink.com>
2016-07-30 11:06:47 -0400

    Fix rubocop ABC size warnings to use rubocop default

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,15 +5,7 @@ class OrganizationsController < ApplicationController
 
   def show
     @org = Organization.friendly.find(params[:id])
-    session[:current_campaign] ||= @org.current_campaign.nil? ? nil : @org.current_campaign.id
-    @organization_campaign = if session[:current_campaign].nil?
-                               nil
-                             else
-                               OrganizationCampaign.where(
-                                 organization_id: @org.id,
-                                 campaign_id: session[:current_campaign]
-                               ).first
-                             end
+    @organization_campaign = find_organization_campaign
     @joined_campaigns = @org.campaigns
     @joinable_campaigns = @org.joinable_campaigns
     @organization_campaign_new = OrganizationCampaign.new(organization_id: @org.id)
@@ -22,5 +14,16 @@ class OrganizationsController < ApplicationController
   def switch_current_campaign
     session[:current_campaign] = params[:organization_campaign][:campaign_id]
     redirect_to organization_path(params[:id])
+  end
+
+  private
+
+  def find_organization_campaign
+    session[:current_campaign] ||= @org.current_campaign.nil? ? nil : @org.current_campaign.id
+    return nil if session[:current_campaign].nil?
+    OrganizationCampaign.find_by_organization_id_and_campaign_id(
+      @org.id,
+      session[:current_campaign]
+    )
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,15 @@ class UsersController < ApplicationController
   def show
     @memberships = current_user.memberships
     has_matches = @memberships.any? && @memberships.first.matches.any?
-    if has_matches
-      membership = @memberships.first
-      match = membership.matches.first
-      campaign = match.recipient.organization_campaign.campaign
-      @campaign_date = campaign.donation_deadline
-    else
-      @campaign_date = Date.today
-    end
+    @campaign_date = has_matches ? find_campaign_date(@memberships) : Date.today
+  end
+
+  private
+
+  def find_campaign_date(memberships)
+    membership = memberships.first
+    match = membership.matches.first
+    campaign = match.recipient.organization_campaign.campaign
+    @campaign_date = campaign.donation_deadline
   end
 end
